36c8597a8d5329697d17b695cdbd757302a7150b
rabaal <balcerowskirafal@gmail.com>
2021-04-25 18:35:27 +0200

    docstrings

--- allegro_zad3.py
+++ allegro_zad3.py
@@ -1,50 +1,53 @@
 #!/usr/bin/python3
 
-# from flask import Flask
-# from flask import render_template
-# from flask import request, redirect, url_for, flash
 import flask
 import requests
 
 
-# todo: class ServerApplication
-
-
 app = flask.Flask(__name__)
 
-def github_request(username: str):
+
+def github_request(username: str) -> list:
     """
     Sends the request to GitHub.
     :param username: The name of a GitHub user.
-    :return: todo: What returned? json (list of dicts?)
+    :return: JSON data as a Python list.
     """
 
     url = f"https://api.github.com/users/{username}/repos"
 
     response = requests.get(url)
 
-    content = response.content
     json_list = response.json()  # list
-    raw = response.raw
-    text = response.text
 
     return json_list
 
 
-def prepare_repos_list(json_list):
+def prepare_repos_list(json_list: list) -> str:
+    """
+    Extracts the necessary data (names and stars of repos).
+    :param json_list: JSON data (list) to extract the necessary data.
+    :return: JSON data (str).
+    """
 
     repos_stars = []
     for repo in json_list:
         name = repo['name']
         stars = repo['stargazers_count']
-        repos_stars.append({name: stars})
+        repos_stars.append({"name": name,
+                            "stars": stars})
 
-    repos_stars = flask.json.dumps(repos_stars)
+    json_data = flask.json.dumps(repos_stars)
 
-    return repos_stars  # todo: i tak zamieniam to potem na str? jest sens używania modułu json? (json to jest string!)
+    return json_data
 
 
-def prepare_stars_sum(json_list):
+def prepare_stars_sum(json_list: list) -> str:
+    """
+    Extracts the necessary data (sum of stars in repos).
+    :param json_list: JSON data (list).
+    :return: JSON data (str).
+    """
 
     starssum = sum(repo["stargazers_count"] for repo in json_list)
 
@@ -55,7 +58,9 @@ def prepare_stars_sum(json_list):
 
 @app.route('/<username>/starssum')
 def stars_sum(username):
-
+    """
+    Endpoint. The sum of the stars for the user.
+    """
     github_response = github_request(username)
 
     try:
@@ -70,7 +75,9 @@ def stars_sum(username):
 
 @app.route('/<username>')
 def repos_list(username):
-
+    """
+    Endpoint. The list of repository names and number of stars.
+    """
     github_response = github_request(username)
 
     try:
@@ -83,31 +90,39 @@ def repos_list(username):
 
 
 @app.errorhandler(404)
-def resource_not_found(e):
+def error404(e):
+    """
+    Resource not found.
+    """
 
     json_data = flask.jsonify(error=str(e))
     return json_data, 404
 
 
 @app.route('/', methods=['GET', 'POST'])
-def index():
+def form():
+    """
+    Form page.
+    """
 
     if flask.request.method == 'POST':
 
         formdata = flask.request.form.items()
         username = next(formdata)[1]
+        list_or_sum = next(formdata)[1]
 
-        return flask.redirect(f'/{username}')
+        if list_or_sum == 'list':
+            return flask.redirect(f'/{username}')
+        else:
+            return flask.redirect(f'/{username}/starssum')
 
-    return '''<form method="POST"> <input name="username" type="text">
-    <input type="submit">
-    </form>
-    '''
+    return flask.render_template('form.html', title="GitHub users' repositories")
 
 
 if __name__ == '__main__':
 
     app.run(debug=True)
+    # app.run(debug=False)
 
 
 #
